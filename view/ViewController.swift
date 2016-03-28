441cef932c096f7d3ad79cfd1408dfdf6ade1f86
911050151 <20131105805lijianqing@bieliangewifi.local>
2016-03-28 20:38:04 +0800

    计算器
    基本代码

--- view/ViewController.swift
+++ view/ViewController.swift
@@ -12,11 +12,12 @@ import UIKit
 class ViewController: UIViewController {
     //@IBOutlet weak var view: UILabel!
     
+    @IBOutlet weak var operatorLaby: UILabel!
     //@IBOutlet weak var result: UILabel!
     @IBOutlet weak var resultLaby: UILabel!
-    var operand1:String=""
-    var operand2:String=""
-    var operand:String=""
+    var operanD1:String=""
+    var operanD2:String=""
+    var operanD:String=""
     
     //@IBOutlet weak var abc: UILabel!
     /*
@@ -50,40 +51,70 @@ class ViewController: UIViewController {
     }
     
     @IBAction func button(sender: UIButton) {
-        let value=sender.currentTitle
-        if (value=="+"||value=="-"||value=="*"||value=="/"){
-            operator=value
-            return
-        }
-        else if value=="="
+        var value = sender.currentTitle
+        let bool_value = true
+        if  value == "+"||value == "-"||value == "*"||value == "÷"||value=="%"||value == "^"
         {
-            var result=0
-            switch operator{
-                case"+":
-                  result=operand1.toInt()!+operand2.toInt()!
-                case"-":
-                  result=operand1.toInt()!-operand2.toInt()!
-                case"*":
-                  result=operand1.toInt()!*operand2.toInt()!
-                case"/":
-                result=operand1.toInt()!/operand2.toInt()!
-                default:
-                result=0
-            }
-            resultLaby.text="\(result)"
+            operanD = value!
+            operatorLaby.text = "\(operanD)"
             return
         }
-        if operator==""{
-            operand1=operand1 +value
-            resultLaby.text=operand1
-        }else{
-            operand2=operand2 +value
-            resultLaby.text=operand2
+        else if value == "AC"{
+            value = "0"
+            operanD1 = ""
+            operanD2 = ""
+            operanD = ""
+            resultLaby.text = "0"
+            operatorLaby.text = ""
+            return
         }
-        
-        
+        else if value == "+/-"{
+            if  bool_value ==  true{
+                bool_value == false
+            }
+            else{
+                bool_value == true
+            }
+        }
+        else if value == "="{
+            var result = 0
+            switch operanD {
+            case "+":
+                result = Int(operanD1)! + Int(operanD2)!
+            case "-":
+                result = Int(operanD1)! - Int(operanD2)!
+            case "*":
+                result = Int(operanD1)! * Int(operanD2)!
+            case "÷":
+                result = Int(operanD1)! / Int(operanD2)!
+            case "^":
+                result = Int(operanD1)! ^ Int(operanD1)!
+            case "%":
+                result = Int(operanD1)! % Int(operanD2)!
+                
+            default :
+                operatorLaby.text = "出错请清零"
+            }
+            resultLaby.text  = "\(result)"
+            operatorLaby.text = ""
+            operanD1 = ""
+            operanD2 = ""
+            operanD = ""
+            
+            return
+        }
+        if operanD   == ""{
+            
+            operanD1  = operanD1 + value!
+            resultLaby.text = "\(operanD1)"
+            return
+        }
+        else {
+            
+            operanD2  = operanD2 + value!
+            resultLaby.text = "\(operanD2)"
+            return
+        }
     }
-
-
 }
 
