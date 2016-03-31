e251a2da1425648894409a98e282d297f84f91e8
911050151 <20131105805lijianqing@bieliangewifi.local>
2016-03-31 11:47:55 +0800

    计算器基础
    代码调试 基本可以输入数字，单个数字，但是无法进行运算

--- view/ViewController.swift
+++ view/ViewController.swift
@@ -10,36 +10,17 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    //@IBOutlet weak var view: UILabel!
     
-    @IBOutlet weak var operatorLaby: UILabel!
-    //@IBOutlet weak var result: UILabel!
-    @IBOutlet weak var resultLaby: UILabel!
-    var operanD1:String=""
-    var operanD2:String=""
-    var operanD:String=""
+    @IBOutlet weak var opeRatorLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    //@IBAction func opeRatorLabel(sender: AnyObject) {
+    //}
+    //@IBAction func resultLabel(sender: ) {
+    ///
+    //}
+   // @IBOutlet weak var opeRatorLabel: UILabel!
+   // @IBOutlet weak var resultLabel: UILabel!
     
-    //@IBOutlet weak var abc: UILabel!
-    /*
-    @IBAction func add(sender: UIButton) {
-        var a:Double!=0
-        var b:Double!=0
-        var c:Double!=0
-        if(!x.text!.isEmpty){
-            a=(x.text! as NSString).doubleValue
-        }
-        if(!y.text!.isEmpty){
-            b=(y.text! as NSString).doubleValue
-        }
-        c=a+b
-        z.text="\(c)"
-
-        
-    }
-    @IBOutlet weak var z: UITextField!
-    @IBOutlet weak var y: UITextField!
-    @IBOutlet weak var x: UITextField!
-    */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -51,21 +32,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func button(sender: UIButton) {
+        var opeRand1: String = ""
+        var opeRand2: String = ""
+        var opeRator: String = ""
+        //var result: String=""
+        //print("opeRand2=\(opeRand2)")
         var value = sender.currentTitle
         let bool_value = true
-        if  value == "+"||value == "-"||value == "*"||value == "÷"||value=="%"||value == "^"
+        if  value == "+"||value == "-"||value == "×"||value == "÷"||value == "%"||value == "^"
         {
-            operanD = value!
-            operatorLaby.text = "\(operanD)"
+            opeRator = value!
+            opeRatorLabel.text = "\(opeRator)"
             return
         }
         else if value == "AC"{
             value = "0"
-            operanD1 = ""
-            operanD2 = ""
-            operanD = ""
-            resultLaby.text = "0"
-            operatorLaby.text = ""
+            opeRand1 = ""
+            opeRand2 = ""
+            opeRator = ""
+            resultLabel.text = "0"
+            opeRatorLabel.text = ""
             return
         }
         else if value == "+/-"{
@@ -78,43 +64,66 @@ class ViewController: UIViewController {
         }
         else if value == "="{
             var result = 0
-            switch operanD {
+            switch opeRator {
             case "+":
-                result = Int(operanD1)! + Int(operanD2)!
+                result = Int(opeRand1)! + Int(opeRand2)!
             case "-":
-                result = Int(operanD1)! - Int(operanD2)!
-            case "*":
-                result = Int(operanD1)! * Int(operanD2)!
+                result = Int(opeRand1)! - Int(opeRand2)!
+            case "×":
+                result = Int(opeRand1)! * Int(opeRand2)!
             case "÷":
-                result = Int(operanD1)! / Int(operanD2)!
-            case "^":
-                result = Int(operanD1)! ^ Int(operanD1)!
+                result = Int(opeRand1)! / Int(opeRand2)!
             case "%":
-                result = Int(operanD1)! % Int(operanD2)!
+                result = Int(opeRand1)! % Int(opeRand2)!
+            case "^":
+                result = Int(opeRand1)! ^ Int(opeRand1)!
                 
             default :
-                operatorLaby.text = "出错请清零"
+                opeRatorLabel.text = "出错请清零"
             }
-            resultLaby.text  = "\(result)"
-            operatorLaby.text = ""
-            operanD1 = ""
-            operanD2 = ""
-            operanD = ""
+            resultLabel.text  = "\(result)"
+            opeRatorLabel.text = ""
+            opeRand1 = ""
+            opeRand2 = ""
+            opeRator = ""
             
             return
         }
-        if operanD   == ""{
+        if opeRator   == ""{
             
-            operanD1  = operanD1 + value!
-            resultLaby.text = "\(operanD1)"
+            opeRand1  = opeRand1 + value!
+            opeRatorLabel.text = "\(opeRand1)"
+            //resultLabel.text = "\(result)"
             return
         }
         else {
             
-            operanD2  = operanD2 + value!
-            resultLaby.text = "\(operanD2)"
+            opeRand2  = opeRand2 + value!
+            opeRatorLabel.text = "\(opeRand2)"
+            //esultLabel.text = "\(result)"
             return
         }
     }
 }
+//@IBOutlet weak var abc: UILabel!
+/*
+@IBAction func add(sender: UIButton) {
+var a:Double!=0
+var b:Double!=0
+var c:Double!=0
+if(!x.text!.isEmpty){
+a=(x.text! as NSString).doubleValue
+}
+if(!y.text!.isEmpty){
+b=(y.text! as NSString).doubleValue
+}
+c=a+b
+z.text="\(c)"
+
+
+}
+@IBOutlet weak var z: UITextField!
+@IBOutlet weak var y: UITextField!
+@IBOutlet weak var x: UITextField!
+*/
 
