4f04d3c8f62ec1d3e58d09afd01050e8ba011ae9
911050151 <20131105805lijianqing@bieliangewifi.local>
2016-04-11 21:18:32 +0800

    计算器
    计算器

--- view/ViewController.swift
+++ view/ViewController.swift
@@ -11,11 +11,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var show: UITextField!;
     var opeRand1: String = ""
     var opeRand2: String = ""
     var opeRator: String = ""
-    
+    var dianLopp=false;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,63 +25,102 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    @IBAction func button(sender: UIButton) {
-        
-        var value = sender.currentTitle
-        print(value)
-        if  value == "+"||value == "-"||value == "*"||value == "/"||value == "%"||value == "^"
+    @IBAction func add1(sender: UIButton) {
+        show.text!+="1";
+    }
+    @IBAction func add2(sender: UIButton) {
+        show.text!+="2";
+    }
+    @IBAction func add3(sender: UIButton) {
+        show.text!+="3";
+    }
+    @IBAction func add4(sender: UIButton) {
+        show.text!+="4";
+    }
+    @IBAction func add5(sender: UIButton) {
+        show.text!+="5";
+    }
+    @IBAction func add6(sender: UIButton) {
+        show.text!+="6";
+    }
+    @IBAction func add7(sender: UIButton) {
+        show.text!+="7";
+    }
+    @IBAction func add8(sender: UIButton) {
+        show.text!+="8";
+    }
+    @IBAction func add9(sender: UIButton) {
+        show.text!+="9";
+    }
+    @IBAction func add0(sender: UIButton) {
+        show.text!+="9";
+    }
+    @IBAction func addDain(sender: UIButton) {
+        if(!dianLopp)
         {
-            opeRator = value!
-            
-        }
-        else if value == "="{
-            var result = 0
-            switch opeRator {
-            case "+":
-                result = Int(opeRand1)! + Int(opeRand2)!
-            case "-":
-                result = Int(opeRand1)! - Int(opeRand2)!
-            case "*":
-                result = Int(opeRand1)! * Int(opeRand2)!
-            case "/":
-                result = Int(opeRand1)! / Int(opeRand2)!
-            case "%":
-                result = Int(opeRand1)! % Int(opeRand2)!
-            case "^":
-                result = Int(opeRand1)! ^ Int(opeRand1)!
-                
-            default :
-                result=0
-            }
-                resultLabel.text = "\(result)"
-                opeRand1 = ""
-                opeRand2 = ""
-                opeRator = ""
-            
-            }
-
-        else if value == "AC"{
-            value = "0"
-            opeRand1 = ""
-            opeRand2 = ""
-            opeRator = ""
-            resultLabel.text = ""
-            
-        }
-         if opeRator == ""{
-            
-            opeRand1  = opeRand1 + value!
-            resultLabel.text = "\(opeRand1)"
-        
-        }
-        else {
-            
-            opeRand2  = opeRand2 + value!
-            resultLabel.text = "\(opeRand2)"
-            
+            show.text!+=".";
+            dianLopp=true;
         }
     }
+    @IBAction func delt(sender: UIButton) {
+        show.text!="";
+        opeRand1="";
+        opeRand2="";
+        dianLopp=false;
+    }
+    
+    
+    @IBAction func add(sender: UIButton) {
+        setx();
+        opeRator="+";
+    }
+    @IBAction func jian(sender: UIButton) {
+        setx();
+        opeRator="-";
+    }
+    @IBAction func cheng(sender: UIButton) {
+        setx();
+        opeRator="*";
+    }
+    @IBAction func chu(sender: UIButton) {
+        setx();
+        opeRator="/";
+    }
+    @IBAction func chu100(sender: UIButton) {
+       // var result:Double=0.0;
+        //result = Double(show.text!);
+        
+        
+    }
+    @IBAction func pwd(sender: UIButton) {
+        //show.text!="\(pwd((Double)(show.text!)))";
+    }
+    @IBAction func count(sender: UIButton) {
+        var result = 0.0;
+        opeRand2=(show.text!);
+        switch opeRator {
+        case "+":
+            result = Double(opeRand1)! + Double(opeRand2)!
+        case "-":
+            result = Double(opeRand1)! - Double(opeRand2)!
+        case "*":
+            result = Double(opeRand1)! * Double(opeRand2)!
+        case "/":
+            result = Double(opeRand1)! / Double(opeRand2)!
+            
+        default :
+            result=0
+        }
+        show.text! = "\(result)"
+        opeRand1 = ""
+        opeRand2 = ""
+        opeRator = ""
+    }
+    func setx(){
+        opeRand1=(show.text!);
+        show.text!="";
+        dianLopp=false;
+    }
+    
 }
 
